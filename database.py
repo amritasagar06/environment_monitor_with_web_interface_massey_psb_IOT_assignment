117810adb70e1fb45a905abbb310db58bde559d4
Amrita Sagar <amritasagar2005@gmail.com>
2026-09-10 18:34:04 +0800

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ import sqlite3
 
 # Set up logging to record error messages
 logger = logging.getLogger(__name__)
-
 # Name of the SQLite database file where data will be stored
 DB_PATH = "environment.db"
 # Default min/max alert thresholds for temperature, humidity, and atmospheric pressure
